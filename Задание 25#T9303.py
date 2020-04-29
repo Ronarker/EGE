5c9873641377253b66107e8130f3ccfa3eb743a0
Ronarker <dinosos.007@gmail.com>
2020-04-29 20:55:46 +0300

    Задание 25(ЕГЭ)

---
+++ Задание 25#T9303.py
@@ -0,0 +1,28 @@
+'''
+https://yandex.ru/tutor/subject/tag/problems/?ege_number_id=363&tag_id=19
+'''
+# допускается также использовать две
+# целочисленные переменные j и k
+a = []
+n = 6
+for i in range(0, n):
+    a.append(int(input()))
+j = 999
+k = []
+for i in range(0, n):
+    k.append(a[i] // 100)
+    k.append(a[i] // 10 % 10)
+    k.append(a[i] % 10)
+    if k[0] == k[1] or k[0] == k[2] or k[1] == k[2]:
+        if a[i] < j:
+            j = a[i]
+    k.clear()
+for i in range(0, n):
+    k.append(a[i] // 100)
+    k.append(a[i] // 10 % 10)
+    k.append(a[i] % 10)
+    if k[0] == k[1] or k[0] == k[2] or k[1] == k[2]:
+        print(j)
+    else:
+        print(a[i])
+    k.clear()
---
+++ Задание 25.py
@@ -0,0 +1,9 @@
+'''
+https://labs-org.ru/wp-content/uploads/3kim18.pdf
+'''
+#допускается использовать целочисленных переменных
+#j и k
+а = []
+n = 40
+for і in range(n):
+ a.append(int(input()))

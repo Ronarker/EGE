374aff41e91146a9739e63cdb72145be47402267
Ronarker <dinosos.007@gmail.com>
2020-05-14 21:10:05 +0300

    Задание 27#2(ЕГЭ)

--- Задание 27.py
+++ Задание 27.py
@@ -1,8 +1,8 @@
 '''
 https://yandex.ru/tutor/subject/tag/problems/?ege_number_id=365&tag_id=19
 '''
+n = int(input())
 m = 120
-n = 7
 a = []
 b = 0
 c = 0
@@ -11,10 +11,8 @@ for i in range(n):
     a.append(int(input()))
 for i in range(n - 1):
     for k in range(i + 1, n):
-        if  (a[i] + a[k]) % 120 == 0:
-            if (a[i] + a[k]) > b + c:
-                d = a[i] + a[k]
-                if i < k and a[i] > a[k]:
-                    b = a[i]
-                    c = a[k]
+        if  (a[i] + a[k]) % m == 0 and (a[i] + a[k]) > b + c:
+            if a[i] > a[k]:
+                c = a[k]
+                b = a[i]
 print(b, c)

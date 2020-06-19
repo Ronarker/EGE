22983c76367e838973831521b81f425a6fa1b25b
Ronarker <55349301+Ronarker@users.noreply.github.com>
2020-06-19 14:10:52 +0300

    Update solution.py

--- INF-EGE/С4/Task9708/solution.py
+++ INF-EGE/С4/Task9708/solution.py
@@ -1,13 +1,12 @@
 # Python 3.6
 N = int(input())
 a = []
-for i in range(N):
-    a.append(int(input()))
 X = 0 # наибольшее число, кратное 14 и являющееся произведением двух элементов последовательности
 for i in range(N):
-    k = N - 1
-    if a[i] != a[k - i]:
-        if (a[i] * a[k - i]) % 14 == 0 and a[i] * a[k - i] > X:
-            X = a[i] * a[k - i]
+    a.append(int(input()))
+    if len(a) > 1:
+        for k in range(i):
+            if a[i] * a[k] % 14 == 0 and a[i] * a[k] > X:
+                X = a[i] * a[k]
 print(X)
 # программа является решением задания Б

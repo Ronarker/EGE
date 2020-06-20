96d0ec063cc335a410ca824dae4a83656997baa0
Ronarker <55349301+Ronarker@users.noreply.github.com>
2020-06-20 12:11:42 +0300

    Update solution.py

--- INF-EGE/С4/Task5503/solution.py
+++ INF-EGE/С4/Task5503/solution.py
@@ -1,19 +1,19 @@
 # Python 3.6
 N = int(input())
 a = []
-r = 'False'
+r = False
 for i in range(N):
     a.append(int(input()))
 R = int(input())
 if R % 14 != 0:
-    r = 'False' # не надо в кавычках писать, это булевский тип в python просто пиши r = False
+    r = False
 for i in range(N - 1):
-    if r == 'False':
+    if r == False:
         for k in range(i + 1, N):
             if a[i] * a[k] == R:
-                r = 'True'
+                r = True
                 print(R)
                 break
-if r == 'False': # Подсказка, булевский тип int(True) == 1 и int(False) == 0
+if r == False: # Подсказка, булевский тип int(True) == 1 и int(False) == 0
     print(0)
 # Программа является решением задания А

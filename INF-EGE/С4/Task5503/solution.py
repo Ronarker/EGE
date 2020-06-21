240fea780d187fa8e2a203e2a61415130bbe2b8f
Vitaliy <potyson@gmail.com>
2020-06-22 00:18:18 +0300

    К задачам С4 5503 и 9708 добавил комментарии

--- INF-EGE/С4/Task5503/solution.py
+++ INF-EGE/С4/Task5503/solution.py
@@ -5,15 +5,16 @@ r = False
 for i in range(N):
     a.append(int(input()))
 R = int(input())
-if R % 14 != 0:
+if R % 14 != 0: # Зачем это? У тебя r  и так равен False
     r = False
 for i in range(N - 1):
     if r == False:
         for k in range(i + 1, N):
-            if a[i] * a[k] == R:
+            if a[i] * a[k] == R: # Тебе надо заново найти R и сравнить его с введенным значением R
                 r = True
                 print(R)
                 break
-if r == False: # Подсказка, булевский тип int(True) == 1 и int(False) == 0
+if r == False: # Не пиши так, лучше просто if not r: без == False,  так как if и так проверяет r == True, а r == False Это not False == True
     print(0)
+# Читай условие! Написано выведите сообщение "Вычесленное конрольное значение" и пройден контроль или нет
 # Программа является решением задания А

0ef18585706bd737fb0051e31472ff2baf896838
Vitaliy <potyson@gmail.com>
2020-06-22 21:05:55 +0300

    Решение задания 9708

--- INF-EGE/С4/Task9708/solution.py
+++ INF-EGE/С4/Task9708/solution.py
@@ -1,13 +1,22 @@
 # Python 3.6
 N = int(input())
-a = []
 X = 0  # наибольшее число, кратное 14 и являющееся произведением двух элементов последовательности
+
+a14 = 0
+a7 = 0
+a2 = 0
+a_max = 0
 for i in range(N):
-    a.append(int(input()))
-    if len(a) > 1:
-        for k in range(i):
-            if a[i] * a[k] % 14 == 0 and a[i] * a[k] > X:
-                X = a[i] * a[k]
+    a = int(input())
+    if a % 14 == 0:
+        a14 = a
+    if a > a_max:
+        a_max = a
+    if a % 2 == 0 and a % 7 != 0 and a2 < a:
+        a2 = a
+    if a % 7 == 0 and a % 2 != 0 and a2 < a:
+        a2 = a
+
+X = a14 * a_max if a14 * a_max > a7 * a2 else a7 * a2
 print(X)
 # программа является решением задания Б
-# Жду оптимальное решение, подсказка проверяй числа на делители числа 14
